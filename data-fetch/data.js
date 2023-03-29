81a7fc61567ffb42ab651cb68ff8bf87bc7da79c
kumarAnunay <kumaranunay12@gmail.com>
2023-03-30 03:21:29 +0530

    add

---
+++ data-fetch/data.js
@@ -0,0 +1,62 @@
+let data = [
+    {
+        id : 1,
+        item : "Car",
+        img : "./image1.jpg",
+        price: 1000000
+    },
+    {
+        id : 2,
+        item : "Car",
+        img : "./image2.jpg",
+        price: 1000000
+    },
+    {
+        id : 3,
+        item : "Car",
+        img : "./image1.jpg",
+        price: 1000000
+    },
+    {
+        id : 4,
+        item : "Car",
+        img : "./image2.jpg",
+        price: 1000000
+    },
+    {
+        id : 5,
+        item : "Car",
+        img : "./image1.jpg",
+        price: 1000000
+    },
+    {
+        id : 6,
+        item : "Car",
+        img : "./image2.jpg",
+        price: 1000000
+    },
+    {
+        id : 7,
+        item : "Car",
+        img : "./image1.jpg",
+        price: 1000000
+    },
+    {
+        id : 8,
+        item : "Car",
+        img : "./image2.jpg",
+        price: 1000000
+    },
+    {
+        id : 9,
+        item : "Car",
+        img : "./image1.jpg",
+        price: 1000000
+    },
+    {
+        id : 10,
+        item : "Car",
+        img : "./image2.jpg",
+        price: 1000000
+    },
+]
